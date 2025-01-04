572a0cc1dac6939dc533db5c780b75c5c14b02db
AlissaStorm <Alissaalidib@hotmail.com>
2025-01-04 22:02:23 +0100

    finished assignment, added error handling

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,12 @@ public class Main {
     public static void main(String[] args) {
         RickshawService r = new RickshawService();
 
-        int[] districts1 = new int[]{1, 2};
-        int[] districts2 = new int[]{2, 3};
+        int[] districtsCoverage = new int[]{1, 2};
+        int[] districtsCoverage2 = new int[]{2, 3};
 
-        r.createDriver("Pete",districts1);
+        r.createDriver("Pete",districtsCoverage);
+        r.createDriver("David",districtsCoverage2);
+
         r.bookDriver();
     }
 }
--- src/RickshawService.java
+++ src/RickshawService.java
@@ -3,39 +3,43 @@ import java.util.Scanner;
 
 public class RickshawService {
     private Scanner scan = new Scanner(System.in);
+
     private String[][] destinations = {{"Christiania", "Operaen", "Havnebad Islands Brygge"}, {"Tivoli", "Rådhuspladsen", "Christiansborg"}, {"Amalienborg", "Strøget", "Nyhavn"}};
     private ArrayList<Driver> drivers = new ArrayList<>();
 
     public void createDriver(String name, int[] destinationCoverage) {
         Driver driver = new Driver(name, destinationCoverage);
         drivers.add(driver);
-
     }
 
     public void bookDriver() {
-        System.out.println("Pick a destination");
-        int counter = 1;
-        for (int i = 0; i < destinations.length; i++) {
-            for (int j = 0; j < destinations[i].length; j++) {
-                System.out.println(counter + ". " + destinations[i][j]);
-                counter++;
+        boolean state = true;
+
+        while (state) {
+            System.out.println("Pick a destination");
+            int counter = 1;
+            for (int i = 0; i < destinations.length; i++) {
+                for (int j = 0; j < destinations[i].length; j++) {
+                    System.out.println(counter + ". " + destinations[i][j]);
+                    counter++;
+                }
+            }
+            int choice = scan.nextInt();
+
+            int districtIndex = (choice - 1) / 3; // Calculate districtIndex
+            int destinationIndex = (choice - 1) % 3; // Calculate destinationIndex
+
+            if (choice >= 1 && choice <= 9) {
+                System.out.println("You have chosen " + destinations[districtIndex][destinationIndex]);
+                String checkIfDriverIsAvailable = findDriver(districtIndex + 1);
+                System.out.println(checkIfDriverIsAvailable);
+                state = false;
+            } else {
+                System.out.println("Invalid input, try again");
             }
         }
-        int choice = scan.nextInt();
-
-        int districtIndex = (choice - 1) / 3; // Beregner distriktsindeks
-        int destinationIndex = (choice - 1) % 3; // Beregner destinationsindeks
-
-        if (choice >= 1 && choice <= 9) {
-            System.out.println("You have chosen " + destinations[districtIndex][destinationIndex]);
-        } else {
-            System.out.println("Invalid input, try again");
-            bookDriver();
-        }
-
-        String checkIfDriverIsAvailable = findDriver(districtIndex + 1);
-        System.out.println(checkIfDriverIsAvailable);
     }
+
 
 
     private String findDriver(int districtNumber) {
